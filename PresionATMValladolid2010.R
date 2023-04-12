409137984e29f0ef1a114cc275c2f33e8567709d
LAACit <314340609@enesmerida.unam.mx>
2023-04-12 17:55:22 -0600

    Datos preision valladolid 2010

---
+++ PresionATMValladolid2010.R
@@ -0,0 +1,24 @@
+#install.packages("lubridate")
+#install.packages("gridExtra")
+library(gridExtra)
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(lubridate)
+
+PresionValla= read.csv("../DatosMAnglar/PresionesYuctán/CGSMN-B00.8.-23-0000154/Valladolid2010Corregido2.csv")
+
+
+
+PresionValla$Fecha.Tiempo <-as.POSIXct(strptime(PresionValla$Fecha.Tiempo, format="%d/%m/%Y %I:%M:%S %p"))
+
+PresionDiciembre <- PresionValla[15600:16318, ]
+
+PresionPromedioDiciembreValla <- mean(PresionDiciembre$BP.mbar.)
+
+GraficaPATMValla<- ggplot(PresionDiciembre, aes(x = PresionDiciembre$Fecha.Tiempo, y = PresionDiciembre$BP.mbar.)) + 
+  geom_line()+
+  xlab("")+
+  ylab("Presión (mbar) ")+
+  ggtitle("Presión Atmosferica Valladolid ")+
+  theme_bw()
