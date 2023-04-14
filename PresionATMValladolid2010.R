f3fd045eaf981194c2ce7dca96ee16d1affbf1a9
LAACit <314340609@enesmerida.unam.mx>
2023-04-13 20:19:42 -0600

    Se arregla grafica y se borran datos no utilizados

--- PresionATMValladolid2010.R
+++ PresionATMValladolid2010.R
@@ -8,17 +8,18 @@ library(lubridate)
 
 PresionValla= read.csv("../DatosMAnglar/PresionesYuctán/CGSMN-B00.8.-23-0000154/Valladolid2010Corregido2.csv")
 
-
-
 PresionValla$Fecha.Tiempo <-as.POSIXct(strptime(PresionValla$Fecha.Tiempo, format="%d/%m/%Y %I:%M:%S %p"))
 
 PresionDiciembre <- PresionValla[15600:16318, ]
 
 PresionPromedioDiciembreValla <- mean(PresionDiciembre$BP.mbar.)
 
-GraficaPATMValla<- ggplot(PresionDiciembre, aes(x = PresionDiciembre$Fecha.Tiempo, y = PresionDiciembre$BP.mbar.)) + 
+ggplot(PresionDiciembre, aes(x = PresionDiciembre$Fecha.Tiempo, y = PresionDiciembre$BP.mbar.)) + 
   geom_line()+
   xlab("")+
   ylab("Presión (mbar) ")+
   ggtitle("Presión Atmosferica Valladolid ")+
   theme_bw()
+
+rm(PresionValla)
+rm(PresionDiciembre)
